26cd1a969ffc73c3cfaf4bc05096734139ae2d17
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-20 14:52:43 +0700

    test config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = function () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +24,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
