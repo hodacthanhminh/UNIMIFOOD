8f2fe57c420d060d3f20b9681d422f87965466ca
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-21 12:11:25 +0700

    Routes uppdate

--- src/Config/routes.jsx
+++ src/Config/routes.jsx
@@ -1,11 +1,21 @@
 // libs
 // components
 import Login from '../pages/Login';
+import Register from '../pages/Register';
+import LandingPage from '../pages/LandingPage';
 
 const routes = [
   {
     path: '/account/login',
     component: Login,
+  },
+  {
+    path: '/account/register',
+    component: Register,
+  },
+  {
+    path: '/',
+    component: LandingPage,
   },
 ];
 
