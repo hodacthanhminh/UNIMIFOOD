6066712938ccd69128ac11c21282fd5d535ad9b5
hodacthanhminh <hodacthanhminh@gmail.com>
2021-12-11 00:38:31 +0700

    create useStoreTime hook && add tagsDatasource

--- src/dataSources/TagStore/index.js
+++ src/dataSources/TagStore/index.js
@@ -1,33 +1,41 @@
 export const tagsValue = {
   'tra-sua': {
+    id: 4,
     label: 'Trà sữa',
     value: 'magenta',
   },
   'an-vat': {
+    id: 10,
     label: 'Ăn vặt',
     value: 'volcano',
   },
   'bua-toi': {
+    id: 8,
     label: 'Bữa tối',
     value: 'purple',
   },
   'bua-sang': {
+    id: 6,
     label: 'Bữa sáng',
     value: 'red',
   },
   'bua-trua': {
+    id: 7,
     label: 'Bữa trưa',
     value: 'orange',
   },
   'ca-phe': {
+    id: 5,
     label: 'Cà phê',
     value: 'gold',
   },
   'trai-cay': {
+    id: 9,
     label: 'Trái cây',
     value: 'green',
   },
   'cua-hang': {
+    id: 11,
     label: 'Cửa hàng',
     value: 'default',
   },
@@ -42,4 +50,46 @@ export const tags = [
   'bua-sang',
   'ca-phe',
   'cua-hang',
+];
+export const storeCategories = [
+  {
+    id: 4,
+    name: 'Trà sữa',
+    slug: 'tra-sua',
+  },
+  {
+    id: 5,
+    name: 'Cà phê',
+    slug: 'ca-phe',
+  },
+  {
+    id: 6,
+    name: 'Bữa sáng',
+    slug: 'bua-sang',
+  },
+  {
+    id: 7,
+    name: 'Bữa trưa',
+    slug: 'bua-trua',
+  },
+  {
+    id: 8,
+    name: 'Bữa tối',
+    slug: 'bua-toi',
+  },
+  {
+    id: 9,
+    name: 'Trái cây',
+    slug: 'trai-cay',
+  },
+  {
+    id: 10,
+    name: 'Ăn vặt',
+    slug: 'an-vat',
+  },
+  {
+    id: 11,
+    name: 'Cửa hàng',
+    slug: 'cua-hang',
+  },
 ];
