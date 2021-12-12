b8969569cca8ed398735ed814c252912c75308e4
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-12-12 16:29:59 +0700

    edit order view

--- src/pages/Store/components/StoreOrderBody/index.jsx
+++ src/pages/Store/components/StoreOrderBody/index.jsx
@@ -1,22 +1,43 @@
 // libs
-import React from 'react';
-import { Button } from 'antd';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button, Divider } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 // components
 import StoreOrderCart from '../StoreOrderCart';
 
-const StoreOrderBody = () => (
-  <div className="store-order-body-wrapper">
-    <div className="store-order-body-wrapper-inner">
-      <StoreOrderCart />
-      <Button
-        className="store-order-body-button"
-        icon={<ShoppingCartOutlined />}
-      >
-        Đặt hàng
-      </Button>
+const StoreOrderBody = ({ totalCart }) => {
+  useEffect(() => {}, [totalCart]);
+  return (
+    <div className="store-order-body-wrapper">
+      <div className="store-order-body-wrapper-inner">
+        <StoreOrderCart />
+        <Divider />
+        <div className="store-order-body-total">
+          <span className="store-order-body-total-label"> Total</span>
+          <span className="store-order-body-total-price">
+            {totalCart}
+            <span>đ</span>
+          </span>
+        </div>
+        <Button
+          className="store-order-body-button"
+          icon={<ShoppingCartOutlined />}
+        >
+          Đặt hàng
+        </Button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default StoreOrderBody;
+StoreOrderBody.propTypes = {
+  totalCart: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  totalCart: state.storeid.totalCart,
+});
+
+export default connect(mapStateToProps, null)(StoreOrderBody);
